02cf228b3ccab72e05fdc62b47591c6a0288fc7b
DuyHieu2004 <doanhieu.11052004@gmail.com>
2026-03-05 19:45:10 +0700

    feature: add swagger and execute recommend course by user-based collaborative filtering

--- Program.cs
+++ Program.cs
@@ -1,6 +1,28 @@
-var builder = WebApplication.CreateBuilder(args);
+﻿var builder = WebApplication.CreateBuilder(args);
+
+// 1. Thêm các Controllers vào hệ thống
+builder.Services.AddControllers();
+
+// 2. KHÚC NÀY ĐỂ BẬT SWAGGER NÈ
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// 3. CẤU HÌNH HIỂN THỊ GIAO DIỆN SWAGGER
+// Thường chỉ bật Swagger khi đang code (Development) để bảo mật
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "API Gợi ý Khóa học v1");
+        c.RoutePrefix = string.Empty; // Mở web lên là hiện luôn Swagger ở trang chủ (localhost:port)
+    });
+}
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
 
 app.Run();
